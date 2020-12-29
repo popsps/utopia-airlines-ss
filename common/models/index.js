49f75bd276e98bf3bdff0745d451ba68f7f1407c
Wesley Clements <wesley.clements@gmail.com>
2020-12-29 15:04:10 -0800

    added aliases to association

--- common/models/index.js
+++ common/models/index.js
@@ -2,10 +2,10 @@ const UserRole = require("./UserRole");
 const User = require("./User");
 const UserInfo = require("./UserInfo");
 
-UserRole.hasMany(User, {foreignKey: {name:"roleId", field: "role_id", allowNull: false}});
-User.belongsTo(UserRole, {foreignKey: {name:"roleId", field: "role_id", allowNull: false}});
+UserRole.hasMany(User, {foreignKey: {name:"roleId", field: "role_id", allowNull: false}, as: "users"});
+User.belongsTo(UserRole, {foreignKey: {name:"roleId", field: "role_id", allowNull: false}, as: "role"});
 
-User.hasOne(UserInfo, {foreignKey: {name:"userId", field: "user_id", allowNull: false} });
+User.hasOne(UserInfo, {foreignKey: {name:"userId", field: "user_id", allowNull: false}, as: "info" });
 UserInfo.belongsTo(User, {foreignKey: {name:"userId", field: "user_id", allowNull: false} });
 
 module.exports = {
