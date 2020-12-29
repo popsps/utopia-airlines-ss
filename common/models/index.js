5452ca8d4fffb11d35a1b77d86b358410bc450af
Wesley Clements <wesley.clements@gmail.com>
2020-12-29 12:19:21 -0800

    added association

--- common/models/index.js
+++ common/models/index.js
@@ -1,10 +1,15 @@
-const UserRole = require("./UserRole")
-const User = require("./User")
-const UserInfo = require("./UserInfo")
+const UserRole = require("./UserRole");
+const User = require("./User");
+const UserInfo = require("./UserInfo");
 
+UserRole.hasMany(User, {foreignKey: {name:"role_id", allowNull: false}});
+User.belongsTo(UserRole);
+
+User.hasOne(UserInfo, {foreignKey: {name:"user_id", allowNull: false} });
+UserInfo.belongsTo(User);
 
 module.exports = {
   UserRole,
   User,
   UserInfo
-}
+};
