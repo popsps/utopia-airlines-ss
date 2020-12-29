5fab796f413c6b4c4bd831e8a093828a57cda352
Wesley Clements <wesley.clements@gmail.com>
2020-12-29 12:01:56 -0800

    added userInfo model

---
+++ common/models/UserInfo.js
@@ -0,0 +1,30 @@
+const {Model, DataTypes} = require("sequelize");
+
+class UserInfo extends Model {}
+
+UserInfo.init({
+  givenName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  familyName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  }
+}, {
+  tableName: "user_info",
+  timestamps: false,
+  freezeTableName: true
+})
+
+module.exports = UserInfo;
