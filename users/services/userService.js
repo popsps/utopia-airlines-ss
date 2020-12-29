5c1027336667fbc73a2aeded0b35f8a0e8621289
Wesley Clements <wesley.clements@gmail.com>
2020-12-29 15:33:52 -0800

    updated to use aliases

--- users/services/userService.js
+++ users/services/userService.js
@@ -1,42 +1,50 @@
-const {User, UserRole, UserInfo} = require("@utopia-airlines-wss/common/models");
-const {NotFoundError} = require("@utopia-airlines-wss/common/errors");
+const { User, UserInfo } = require("@utopia-airlines-wss/common/models");
+const { NotFoundError } = require("@utopia-airlines-wss/common/errors");
 
 const userService = {
-  async findAllUsers({roleId} = {}) {
+  async findAllUsers({ roleId } = {}) {
     const query = {};
     if (roleId != null) query.roleId = roleId;
-    return User.findAll({where: query, include: [UserRole, UserInfo]});
+    return User.findAll({
+      where: query,
+      include: ["role", "info"],
+    });
   },
   async findUserById(userId) {
-    const user = await User.findByPk(userId, {include: [UserRole, UserInfo]});
+    const user = await User.findByPk(userId, { include: ["role", "info"] });
     if (!user) throw new NotFoundError("cannot find user");
     return user;
   },
-  async createUser({roleId, username, password, userInfo} = {}) {
-    const user = User.create({roleId, username, password});
-    if (userInfo != null) {
-      const {givenName, familyName, email, phone} = userInfo;
-      UserInfo.create({userId: user.id, givenName, familyName, email, phone});
+  async createUser({ roleId, username, password, info } = {}) {
+    const user = await User.create({ roleId, username, password });
+    if (info != null) {
+      const { givenName, familyName, email, phone } = info;
+      await UserInfo.create({
+        userId: user.id,
+        givenName,
+        familyName,
+        email,
+        phone,
+      });
     }
+    return user;
   },
-  async updateUser(userId, {roleId, password, userInfo}) {
-    try {
-      const user = await userService.findUserById(userId);
-      if (!user) throw new NotFoundError("cannot find user");
-      if (roleId != null)
-        user.roleId = roleId;
-      if (password != null)
-        user.password = password;
-      const promises = [user.save()];
-      if (userInfo != null) {
-        const {givenName, familyName, email, phone} = userInfo;
-        promises.push(UserInfo.upsert({userId,givenName, familyName, email, phone }));
-      }
-      await Promise.all(promises);
-      return;
-    } catch (err) {
-      console.log(err);
+  async updateUser(userId, { roleId, username, password, userInfo }) {
+    const user = await userService.findUserById(userId);
+    if (!user) throw new NotFoundError("cannot find user");
+    if (roleId != null)
+      user.roleId = roleId;
+    if (username != null)
+      user.username = username;
+    if (password != null)
+      user.password = password;
+    const promises = [user.save()];
+    if (userInfo != null) {
+      const { givenName, familyName, email, phone } = userInfo;
+      promises.push(UserInfo.upsert({ userId,givenName, familyName, email, phone }));
     }
+    await Promise.all(promises);
+    return;
   },
   async deleteUser(userId) {
     const user = await User.findByPk(userId);
