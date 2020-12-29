7819ec3d9dfba6d880cdcb3871fb2e26788371b9
Wesley Clements <wesley.clements@gmail.com>
2020-12-29 15:22:58 -0800

    added console for unexpected errors

--- common/middleware/errorHandler.js
+++ common/middleware/errorHandler.js
@@ -4,6 +4,7 @@ module.exports = ( err, req, res, _next ) => {
   if (err instanceof StandardizedError) {
     res.status(err.status).json(err);
   } else {
+    console.error(err);
     res.status(500).send({ message: err.message });
   }
 };
