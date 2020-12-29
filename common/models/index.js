0c980ccefcd9079baf239805aeba03206c481faa
Wesley Clements <wesley.clements@gmail.com>
2020-12-29 14:31:14 -0800

    fixed bad field name

--- common/models/index.js
+++ common/models/index.js
@@ -2,7 +2,7 @@ const UserRole = require("./UserRole");
 const User = require("./User");
 const UserInfo = require("./UserInfo");
 
-UserRole.hasMany(User, {foreignKey: {name:"roleId", field: "user_id", allowNull: false}});
+UserRole.hasMany(User, {foreignKey: {name:"roleId", field: "role_id", allowNull: false}});
 User.belongsTo(UserRole);
 
 User.hasOne(UserInfo, {foreignKey: {name:"userId", field: "user_id", allowNull: false} });
