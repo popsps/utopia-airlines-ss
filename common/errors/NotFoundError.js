04551c9571c2829579845ef1db2c8efd050adb90
Wesley Clements <wesley.clements@gmail.com>
2020-12-29 14:54:59 -0800

    added spread

--- common/errors/NotFoundError.js
+++ common/errors/NotFoundError.js
@@ -2,7 +2,7 @@ const { StandardizedError } = require("./StandardizedError");
 
 class NotFoundError extends StandardizedError {
   constructor(...resources) {
-    super("Not Found", 404, resources.map(resource => ({message: resource})));
+    super("Not Found", 404, ...resources.map(resource => ({message: resource})));
     Object.setPrototypeOf(this, NotFoundError.prototype);
   }
 }
