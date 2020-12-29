8e3301693fec6fc6d76020dcb95030f57d631bfb
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2020-12-29 18:36:09 -0500

    Add Booking layout + Tweak schema

--- bookings/index.js
+++ bookings/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const BookingController = require("./controller/bookingController");
 
-const PORT = 3000;
-
+const PORT = process.env.Port || 3000;
 const app = express();
+app.use(express.json());
 
+app.use("/bookings", BookingController);
 
-app.listen(PORT, () => console.log("App listening on " + PORT));
+app.listen(PORT, () => console.log("Booking controller is running on " + PORT));
---
+++ bookings/model/bookingModel.js
@@ -0,0 +1,12 @@
+const {DataTypes} = require("sequelize");
+
+const Booking = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    field: "id",
+  },
+};
+
+module.exports = Booking;
