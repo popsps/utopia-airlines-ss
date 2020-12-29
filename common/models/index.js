a0495ed54922287544be3866e89e129e4be0f73b
Wesley Clements <wesley.clements@gmail.com>
2020-12-29 14:35:28 -0800

    fixed foreign keys

--- common/models/index.js
+++ common/models/index.js
@@ -3,10 +3,10 @@ const User = require("./User");
 const UserInfo = require("./UserInfo");
 
 UserRole.hasMany(User, {foreignKey: {name:"roleId", field: "role_id", allowNull: false}});
-User.belongsTo(UserRole);
+User.belongsTo(UserRole, {foreignKey: {name:"roleId", field: "role_id", allowNull: false}});
 
 User.hasOne(UserInfo, {foreignKey: {name:"userId", field: "user_id", allowNull: false} });
-UserInfo.belongsTo(User);
+UserInfo.belongsTo(User, {foreignKey: {name:"userId", field: "user_id", allowNull: false} });
 
 module.exports = {
   UserRole,
