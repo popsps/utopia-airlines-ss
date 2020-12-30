61f7352edbdc5a557649359583f53f23fd8a38cc
Wesley Clements <wesley.clements@gmail.com>
2020-12-30 12:30:15 -0800

    spilt schema into two parts

--- users/schemas/user.js
+++ users/schemas/user.js
@@ -1,5 +1,5 @@
-const getRequired = ({ isOptional, errorMessage }) => {
-  if (isOptional) return { optional: true };
+const getRequired = ({ optional, errorMessage }) => {
+  if (optional) return { optional: true };
   else return {
     exists: {
       errorMessage,
@@ -8,104 +8,105 @@ const getRequired = ({ isOptional, errorMessage }) => {
   };
 };
 
-const getUserSchema = (isOptional = false) => {
-  return ({
-    roleId: {
-      optional: true,
-      isInt: {
-        errorMessage: "roleId must be an int",
-        options: { min: 1 },
-      },
+const getUserSchema = ({ optional = false } = {}) => ({
+  roleId: {
+    optional: true,
+    isInt: {
+      errorMessage: "roleId must be an int",
+      options: { min: 1 },
     },
-    username: {
-      ...getRequired({
-        isOptional,
-        errorMessage: "please provide a username",
-      }),
-      isString: {
-        errorMessage: "username must be a string",
-        bail: true,
-      },
-      isLength: {
-        errorMessage: "username must be at least 3 characters long",
-        options: { min: 3 },
-      },
-      isAlphanumeric: {
-        errorMessage: "username must be alphanumeric",
-      },
+  },
+  username: {
+    ...getRequired({
+      optional,
+      errorMessage: "please provide a username",
+    }),
+    isString: {
+      errorMessage: "username must be a string",
+      bail: true,
     },
-    password: {
-      ...getRequired({
-        isOptional,
-        errorMessage: "please provide a password",
-      }),
-      isString: {
-        errorMessage: "password must be a string",
-        bail: true,
-      },
-      isLength: {
-        errorMessage: "password must be at least 8 characters long",
-        options: { min: 8 },
-      },
-      isStrongPassword: {
-        errorMessage: "password must contain at least one lowercase character, one uppercase character, one number, and one special character.",
-        options: { minLength: 1 },
-      },
+    isLength: {
+      errorMessage: "username must be at least 3 characters long",
+      options: { min: 3 },
     },
-    "info.name.given": {
-      ...getRequired({
-        isOptional,
-        errorMessage: "please provide a given name",
-      }),
-      isString: {
-        errorMessage: "given name must be a string",
-        bail: true,
-      },
-      notEmpty: {
-        errorMessage: "please provide a given name",
-      },
+    isAlphanumeric: {
+      errorMessage: "username must be alphanumeric",
     },
-    "info.name.family": {
-      ...getRequired({
-        isOptional,
-        errorMessage: "please provide a family name",
-      }),
-      isString: {
-        errorMessage: "family name must be a string",
-        bail: true,
-      },
-      notEmpty: {
-        errorMessage: "please provide a given name",
-      },
+  },
+  password: {
+    ...getRequired({
+      optional,
+      errorMessage: "please provide a password",
+    }),
+    isString: {
+      errorMessage: "password must be a string",
+      bail: true,
     },
-    "info.email": {
-      ...getRequired({
-        isOptional,
-        errorMessage: "please provide an email",
-      }),
-      isString: {
-        errorMessage: "email must be a string",
-        bail: true,
-      },
-      isEmail: {
-        errorMessage: "please provide a valid email",
-      },
-      normalizeEmail: true,
+    isLength: {
+      errorMessage: "password must be at least 8 characters long",
+      options: { min: 8 },
     },
-    "info.phone": {
-      ...getRequired({
-        isOptional,
-        errorMessage: "please provide a phone number",
-      }),
-      isString: {
-        errorMessage: "phone number must be a string",
-        bail: true,
-      },
-      isMobilePhone: {
-        errorMessage: "please provide a valid phone number",
-      },
+    isStrongPassword: {
+      errorMessage: "password must contain at least one lowercase character, one uppercase character, one number, and one special character.",
+      options: { minLength: 1 },
     },
-  });
-};
+  },
+});
 
-module.exports = getUserSchema;
+const getUserInfoSchema = ({ optional = false } = {}) => ({
+  "info.name.given": {
+    ...getRequired({
+      optional,
+      errorMessage: "please provide a given name",
+    }),
+    isString: {
+      errorMessage: "given name must be a string",
+      bail: true,
+    },
+    notEmpty: {
+      errorMessage: "please provide a given name",
+    },
+  },
+  "info.name.family": {
+    ...getRequired({
+      optional,
+      errorMessage: "please provide a family name",
+    }),
+    isString: {
+      errorMessage: "family name must be a string",
+      bail: true,
+    },
+    notEmpty: {
+      errorMessage: "please provide a given name",
+    },
+  },
+  "info.email": {
+    ...getRequired({
+      optional,
+      errorMessage: "please provide an email",
+    }),
+    isString: {
+      errorMessage: "email must be a string",
+      bail: true,
+    },
+    isEmail: {
+      errorMessage: "please provide a valid email",
+    },
+    normalizeEmail: true,
+  },
+  "info.phone": {
+    ...getRequired({
+      optional,
+      errorMessage: "please provide a phone number",
+    }),
+    isString: {
+      errorMessage: "phone number must be a string",
+      bail: true,
+    },
+    isMobilePhone: {
+      errorMessage: "please provide a valid phone number",
+    },
+  },
+});
+
+module.exports = { getUserSchema, getUserInfoSchema };
