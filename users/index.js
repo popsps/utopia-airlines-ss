f2b4c431b71d6ce1d595b80d0cebfd24301b0c9d
Wesley Clements <wesley.clements@gmail.com>
2020-12-30 11:13:19 -0800

    fixed formatting

--- users/index.js
+++ users/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const {sequelize} = require("@utopia-airlines-wss/common/db");
-const {errorHandler} = require("@utopia-airlines-wss/common/middleware");
+const { sequelize } = require("@utopia-airlines-wss/common/db");
+const { errorHandler } = require("@utopia-airlines-wss/common/middleware");
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +13,6 @@ app.use(require("./routes"));
 
 app.use(errorHandler);
 
-sequelize.sync({force: false}).then (() => {
+sequelize.sync({ force: false }).then (() => {
   app.listen(PORT, () => console.log("App listening on " + PORT));
 });
