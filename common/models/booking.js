83bd3d3d3d4d2d198141ee3c9e25949f92a91062
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2020-12-31 19:12:01 -0500

    add booking and passenger models to common

---
+++ common/models/booking.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+// const { Sequelize, DataTypes, Model } = require("sequelize");
+const Sequelize = require("sequelize").Sequelize;
+
+const Booking = {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    field: "id",
+  },
+  bookerId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    field: "booker_id",
+  },
+  isActive: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    field: "is_active",
+  },
+};
+
+
+module.exports = Booking;
