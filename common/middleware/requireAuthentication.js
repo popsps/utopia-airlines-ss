0106bae5e87ac70924a9537272fb4f7438c4d41e
Wesley Clements <wesley.clements@gmail.com>
2020-12-31 16:58:59 -0800

    passed req ro condition if it is a function

--- common/middleware/requireAuthentication.js
+++ common/middleware/requireAuthentication.js
@@ -1,7 +1,7 @@
 const { AuthenticationError, AuthorizationError } = require("../errors");
 
 const requireAuthentication = ({ roles, errorMessage, condition }) => (req, res, next) => {
-  if (typeof condition === "function"? condition(): condition) {
+  if (typeof condition === "function"? condition(req): condition) {
     if (!res.user) throw new AuthenticationError();
     if (!roles.includes(req.user.role.name)) 
       throw new AuthorizationError(errorMessage);
