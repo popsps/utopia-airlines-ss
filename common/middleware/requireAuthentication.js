cbc97c34e144692309104430e5f14072729ce7bc
Wesley Clements <wesley.clements@gmail.com>
2020-12-31 16:12:40 -0800

    simplified auth middleware

--- common/middleware/requireAuthentication.js
+++ common/middleware/requireAuthentication.js
@@ -1,24 +1,14 @@
-const { Op } = require("sequelize");
 const { AuthenticationError } = require("../errors");
-const { UserRole } = require("../models");
 
 /**
  * 
  * @param  {...string} roles 
  */
-const requireAuthentication = (...roles) => {
-  const roleIds = UserRole.findAll({
-    where: {
-      [Op.or]: roles.map(name => ({ name })),
-    },
-    attributes: ["id"],
-  }).then((userRoles) => userRoles.map(({ id }) => id));
-  return async (req, res, next) => {
-    if (!(await roleIds).includes(req.user?.roleId)) {
-      throw new AuthenticationError();
-    }
-    next();
-  };
+const requireAuthentication = (...roles) => async (req, res, next) => {
+  if (!roles.includes(req.user?.role.name)) {
+    throw new AuthenticationError();
+  }
+  next();
 };
 
 module.exports = { requireAuthentication };
