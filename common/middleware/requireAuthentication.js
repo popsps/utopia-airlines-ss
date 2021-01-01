a7ad9facfe7c9fbdef099998cde08b03dfb06bc3
Wesley Clements <wesley.clements@gmail.com>
2020-12-31 16:56:12 -0800

    added condition

--- common/middleware/requireAuthentication.js
+++ common/middleware/requireAuthentication.js
@@ -1,9 +1,11 @@
 const { AuthenticationError, AuthorizationError } = require("../errors");
 
-const requireAuthentication = ({ roles, errorMessage }) => (req, res, next) => {
-  if (!res.user) throw new AuthenticationError();
-  if (!roles.includes(req.user.role.name)) 
-    throw new AuthorizationError(errorMessage);
+const requireAuthentication = ({ roles, errorMessage, condition }) => (req, res, next) => {
+  if (typeof condition === "function"? condition(): condition) {
+    if (!res.user) throw new AuthenticationError();
+    if (!roles.includes(req.user.role.name)) 
+      throw new AuthorizationError(errorMessage);
+  }
   next();
 };
 
