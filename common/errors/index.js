26ef6a521816ca5c7babf97e66bf4efd40990d26
Wesley Clements <wesley.clements@gmail.com>
2021-01-04 11:40:02 -0800

    added mutation error util

--- common/errors/index.js
+++ common/errors/index.js
@@ -1,8 +1,25 @@
-module.exports = {
+
+const errors = {
   ...require("./StandardizedError"),
   ...require("./NotFoundError"),
   ...require("./BadRequestError"),
   ...require("./AuthenticationError"),
   ...require("./AuthorizationError"),
   ...require("./RequestValidationError"),
+  handleMutationError(err) {
+    if (err.original?.code === "ER_DUP_ENTRY") {
+      throw new errors.StateConflictError(
+        Object.keys(err.fields)
+          .map((path) => path.match(/([^.]*)(?=_UNIQUE$)/)?.[0])
+          .join(", ")
+          + " already exists"
+      );
+    }
+    else {
+      console.error(err);
+      throw new errors.BadRequestError("invalid user data");
+    }
+  },
 };
+
+module.exports = errors;
