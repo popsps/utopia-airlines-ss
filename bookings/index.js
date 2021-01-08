21108f5f2ee23cfb22c14b81fdd462506a274207
Wesley Clements <wesley.clements@gmail.com>
2021-01-07 22:53:42 -0800

    added cookie session

--- bookings/index.js
+++ bookings/index.js
@@ -1,16 +1,23 @@
 require("dotenv").config();
 const express = require("express");
+const cookieSession = require("cookie-session");
 
 const PORT = process.env.PORT || process.argv[2] || 3000;
 
-const { errorHandler } = require("@utopia-airlines-wss/common/middleware");
+const { getCurrentUser, errorHandler } = require("@utopia-airlines-wss/common/middleware");
 
 const app = express();
 
-app.use(express.json());
-
-app.use(require("./routes"));
-
-app.use(errorHandler);
+app.use(
+  express.json(),
+  cookieSession({
+    name: "session",
+    httpOnly: true,
+    signed: false,
+  }),
+  getCurrentUser,
+  require("./routes"),
+  errorHandler
+);
 
 app.listen(PORT, () => console.log("Booking controller is running on " + PORT));
