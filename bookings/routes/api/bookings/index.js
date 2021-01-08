30e76c7b165cb9eb2be781e618c4eef75fb4ca55
Wesley Clements <wesley.clements@gmail.com>
2021-01-08 15:09:24 -0800

    added request validation and authentication

--- bookings/routes/api/bookings/index.js
+++ bookings/routes/api/bookings/index.js
@@ -3,12 +3,58 @@ const router = Router();
 
 const { bookingController } = require("../../../controllers/bookingController");
 
+const { body, oneOf } = require("express-validator");
+const { validateRequest, requireAuthentication } = require("@utopia-airlines-wss/common/middleware");
+
 
 router.get("/", bookingController.getAll);
-router.post("/", bookingController.create);
+router.post("/",
+  [
+    body("flights")
+      .exists().withMessage("please provide flights")
+      .bail()
+      .isArray().withMessage("flights must be an array")
+      .isLength({ min:1 }).withMessage("please provide a flight"),
+    body("passengers")
+      .exists().withMessage("please provide passengers")
+      .bail()
+      .isArray().withMessage("passengers must be an array")
+      .isLength({ min:1 }).withMessage("please provide a passenger"),
+    oneOf([
+      body("contact")
+        .isInt().withMessage("please provide a contact"),
+      [
+        body("contact.contactEmail")
+          .isEmail().withMessage("please provide a contact email"),
+        body("contact.contactPhone")
+          .isEmail().withMessage("please provide a contact phone"),
+      ],
+    ]),
+  ],
+  validateRequest,
+  requireAuthentication({
+    roles: ["AGENT"],
+    condition: (req) => typeof req.body?.contact === "number", 
+  }),
+  requireAuthentication({
+    roles: ["CUSTOMER"],
+    condition: (req) => req.body?.contact == null, 
+  }),
+  bookingController.create
+);
 
-router.get("/:id", bookingController.getById);
-router.put("/:id", bookingController.updateById);
-router.delete("/:id", bookingController.deleteById);
+router.route("/:id")
+  .get(bookingController.getById)
+  .put(
+    [
+      body("flights")
+        .isArray().withMessage("please provide flights"),
+      body("passengers")
+        .isArray().withMessage("please provide passengers"),
+    ],
+    validateRequest,
+    bookingController.updateById
+  )
+  .delete(bookingController.deleteById);
 
 module.exports = router;
