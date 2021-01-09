fa766be5b3c2a05e10179accd133553705554596
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2021-01-09 16:11:17 -0500

    controller base layout

--- authentication-microservice/src/main/java/com/ss/utopia/auth/controller/UserController.java
+++ authentication-microservice/src/main/java/com/ss/utopia/auth/controller/UserController.java
@@ -4,7 +4,10 @@ import com.ss.utopia.auth.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +19,16 @@ public class UserController {
   @Autowired
   public UserController(UserService userService) {
     this.userService = userService;
+  }
+
+  @PostMapping("/signin")
+  public String login() {
+    return null;
+  }
+
+  @PostMapping("/signup")
+  @ResponseStatus(HttpStatus.CREATED)
+  public String signup() {
+    return null;
   }
 }
