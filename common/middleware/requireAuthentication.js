6902fcc47406edacc9c36ab8c64663aeb2969b89
Wesley Clements <wesley.clements@gmail.com>
2021-01-12 11:12:41 -0800

    made role check optional

--- common/middleware/requireAuthentication.js
+++ common/middleware/requireAuthentication.js
@@ -3,7 +3,7 @@ const { AuthenticationError, AuthorizationError } = require("../errors");
 const requireAuthentication = ({ roles, errorMessage, condition }) => (req, res, next) => {
   if (typeof condition === "function"? condition(req): condition) {
     if (!res.user) throw new AuthenticationError();
-    if (!roles.includes(req.user.role.name)) 
+    if (roles && !roles.includes(req.user.role.name)) 
       throw new AuthorizationError(errorMessage);
   }
   next();
