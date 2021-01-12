32533d0293d59b7673e260f782544ea17d273354
Wesley Clements <wesley.clements@gmail.com>
2021-01-12 12:20:53 -0800

    added default if condition is not provided

--- common/middleware/requireAuthentication.js
+++ common/middleware/requireAuthentication.js
@@ -1,7 +1,7 @@
 const { AuthenticationError, AuthorizationError } = require("../errors");
 
-const requireAuthentication = ({ roles, errorMessage, condition } = {}) => (req, res, next) => {
-  if (typeof condition === "function"? condition(req): condition) {
+const requireAuthentication = ({ roles, errorMessage, condition = true } = {}) => (req, res, next) => {
+  if ((typeof condition === "function"? condition(req): condition)) {
     if (!res.user) throw new AuthenticationError();
     if (roles && !roles.includes(req.user.role.name)) 
       throw new AuthorizationError(errorMessage);
