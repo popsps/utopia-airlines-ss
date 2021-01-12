dc7aeba79f744e67d4885af218cc4740367e8855
Wesley Clements <wesley.clements@gmail.com>
2021-01-12 11:13:18 -0800

    added default for param

--- common/middleware/requireAuthentication.js
+++ common/middleware/requireAuthentication.js
@@ -1,6 +1,6 @@
 const { AuthenticationError, AuthorizationError } = require("../errors");
 
-const requireAuthentication = ({ roles, errorMessage, condition }) => (req, res, next) => {
+const requireAuthentication = ({ roles, errorMessage, condition } = {}) => (req, res, next) => {
   if (typeof condition === "function"? condition(req): condition) {
     if (!res.user) throw new AuthenticationError();
     if (roles && !roles.includes(req.user.role.name)) 
