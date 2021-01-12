c31609e207d123940406b2418d21c7387516e386
Wesley Clements <wesley.clements@gmail.com>
2021-01-12 12:27:40 -0800

    fixed checking for user on res

--- common/middleware/requireAuthentication.js
+++ common/middleware/requireAuthentication.js
@@ -2,7 +2,7 @@ const { AuthenticationError, AuthorizationError } = require("../errors");
 
 const requireAuthentication = ({ roles, errorMessage, condition = true } = {}) => (req, res, next) => {
   if ((typeof condition === "function"? condition(req): condition)) {
-    if (!res.user) throw new AuthenticationError();
+    if (!req.user) throw new AuthenticationError();
     if (roles && !roles.includes(req.user.role.name)) 
       throw new AuthorizationError(errorMessage);
   }
