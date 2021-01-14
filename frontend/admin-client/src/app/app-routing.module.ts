0ca1a016907f46997c1cd8f3d32cc5f0e5407a28
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2021-01-14 14:42:25 -0500

    change folder structure

--- frontend/admin-client/src/app/app-routing.module.ts
+++ frontend/admin-client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
-import {BookingComponent} from './components/booking/booking.component';
+import {HomeComponent} from './layout/home/home.component';
+import {BookingComponent} from './booking/booking.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
--- frontend/admin-client/src/app/app.module.ts
+++ frontend/admin-client/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { BookingComponent } from './components/booking/booking.component';
-import { LoginModalComponent } from './components/login-modal/login-modal.component';
+import { HomeComponent } from './layout/home/home.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { BookingComponent } from './booking/booking.component';
+import { LoginModalComponent } from './shared/components/login-modal/login-modal.component';
 
 @NgModule({
   declarations: [
