eef7e841d3c2d26bb97dc5f9e8ebb23f0a30b931
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2021-01-15 13:52:06 -0500

    add deserialization

--- frontend/admin-client/src/app/shared/models/booking.ts
+++ frontend/admin-client/src/app/shared/models/booking.ts
@@ -1,5 +1,12 @@
-export class Booking {
+import {Deserializable} from './Deserializable';
+
+export class Booking implements Deserializable {
   id: bigint;
   isActive: boolean;
   confirmationCode: string;
+
+  deserialize(input: any): this {
+    Object.assign(this, input);
+    return this;
+  }
 }
--- frontend/admin-client/src/app/shared/services/booking.service.ts
+++ frontend/admin-client/src/app/shared/services/booking.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
 import {Booking} from '../models/booking';
 
 @Injectable({
@@ -12,7 +13,11 @@ export class BookingService {
   }
 
   getAllBookings(url: string): Observable<Booking []> {
-    return this.http.get<Booking[]>(url);
+    // return this.http.get<Booking[]>(url).pipe(map(data =>
+    //   new Booking().deserialize(data)), catchError(() => throwError('Cannot')));
+    return this.http.get<Booking[]>(url).pipe(
+      map(rdata => rdata.map(data => new Booking().deserialize(data)))
+    );
   }
 
   getBookingById(url: string, id: number): Observable<Booking> {
