482a18aa674ce2ff9e65e4d11c26f063fe4daac2
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2021-01-15 10:11:11 -0500

    add booking url

--- frontend/admin-client/src/environments/environment.ts
+++ frontend/admin-client/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  bookingApi: 'http://localhost:3000/api/booking'
 };
 
 /*
