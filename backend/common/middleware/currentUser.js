ab9e27908305acbeeef2b81fa243b3cf869b72f2
Wesley Clements <wesley.clements@gmail.com>
2021-01-18 17:54:04 -0800

    changed to mirror auth service jwt payload

--- backend/common/middleware/currentUser.js
+++ backend/common/middleware/currentUser.js
@@ -7,10 +7,12 @@ const getCurrentUser = async (req, res, next) => {
   try {
     if (req.session?.jwt) {
       const payload = await authJWTHandler.verifyJWT(req.session.jwt);
-      req.user = await User.findByPk(payload.id, { include: ["role"] });
+      
+      req.user = await User.findByPk(payload.sub, { include: ["role"] });
     }
     next();
   } catch (err) {
+    console.log({ err });
     next(err);
   }
 };
