8c48843e831a7bbd4928a404c8bbb679ccf2adc1
Wesley Clements <wesley.clements@gmail.com>
2021-01-20 09:53:56 -0800

    fixed typing and flooring issues

--- frontend/admin-client/src/app/shared/pipes/flight-departure-date.pipe.ts
+++ frontend/admin-client/src/app/shared/pipes/flight-departure-date.pipe.ts
@@ -1,9 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Flight } from '../models/Flight';
 
+const toDate = (date: Date | string) => date instanceof Date ? date : new Date(date);
 const floor = (date: Date) => {
   const floored = new Date(date.getTime());
   floored.setHours(0, 0, 0, 0);
+  floored.setDate(floored.getDate() + 1);
   return floored;
 };
 const ceil = (date: Date) => {
@@ -12,21 +14,24 @@ const ceil = (date: Date) => {
   return ceiled;
 };
 const isLessThan = (a: Date, b: Date) => a.getTime() < b.getTime();
-const isBetween = (date: Date, min: Date, max: Date) => isLessThan(floor(min), date) && isLessThan(date, ceil(max));
+const isBetween = (date: Date, min: Date, max: Date) => isLessThan(min, date) && isLessThan(date, max);
 
 @Pipe({
   name: 'flightDepartureDate',
   pure: false
 })
 export class FlightDepartureDatePipe implements PipeTransform {
-
-
-  transform(values: Flight[], date1: Date, date2: Date): Flight[] {
+  transform(values: Flight[], date1: Date | string, date2: Date | string): Flight[] {
     if (date1 != null && date2 != null)
-      return values.filter(flight => isBetween(flight.departureTime, date1, date2));
+    {
+      const [from, to] = [floor(toDate(date1)), ceil(toDate(date2))];
+      return values.filter(flight => isBetween(flight.departureTime, from, to));
+    }
     if (date1 != null && date2 == null)
-      return values.filter(flight => isBetween(flight.departureTime, date1, date1));
+    {
+      const [dayStart, dayEnd] = [floor(toDate(date1)), ceil(toDate(date1))];
+      return values.filter(flight => isBetween(flight.departureTime, dayStart, dayEnd));
+    }
     return values;
   }
-
 }
