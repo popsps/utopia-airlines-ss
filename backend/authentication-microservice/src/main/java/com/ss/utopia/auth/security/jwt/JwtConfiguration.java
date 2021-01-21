178a18820ba1684850eb6a8326d0afcaae44aa70
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2021-01-21 08:33:24 -0500

    fix Parsing public/private key

--- backend/authentication-microservice/src/main/java/com/ss/utopia/auth/security/jwt/JwtConfiguration.java
+++ backend/authentication-microservice/src/main/java/com/ss/utopia/auth/security/jwt/JwtConfiguration.java
@@ -24,8 +24,8 @@ public class JwtConfiguration {
 
   private PrivateKey generatePrivateKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
     final KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-    privateKey = privateKey.replaceAll("-----BEGIN (.*)-----", "").replaceAll("-----END (.*)----", "")
-        .replaceAll("\r\n", "").replaceAll("\n", "").trim();
+    privateKey = privateKey.replaceAll("-----BEGIN (.*?)-----", "").replaceAll("-----END (.*)----", "")
+        .replaceAll("\r\n", "").replaceAll("\\\\n", "").trim();
     final byte[] decodedPrivate = Base64.getDecoder().decode(privateKey);
     final KeySpec keySpec = new PKCS8EncodedKeySpec(decodedPrivate);
     return keyFactory.generatePrivate(keySpec);
@@ -33,8 +33,8 @@ public class JwtConfiguration {
 
   private PublicKey generatePublicKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
     final KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-    publicKey = publicKey.replaceAll("-----BEGIN (.*)-----", "").replaceAll("-----END (.*)----", "")
-        .replaceAll("\r\n", "").replaceAll("\n", "").trim();
+    publicKey = publicKey.replaceAll("-----BEGIN (.*?)-----", "").replaceAll("-----END (.*)----", "")
+        .replaceAll("\r\n", "").replaceAll("\\\\n", "").trim();
     final byte[] decodedPublic = Base64.getDecoder().decode(publicKey);
     final KeySpec keySpec = new X509EncodedKeySpec(decodedPublic);
     return keyFactory.generatePublic(keySpec);
