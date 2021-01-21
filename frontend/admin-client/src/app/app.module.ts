e61a0aefe4e20e807792c6efcc6d402606c4811c
Samuel Johnson <samuel.johnson@smoothstack.com>
2021-01-20 22:47:51 -0700

    User creation done

--- frontend/admin-client/src/app/app.module.ts
+++ frontend/admin-client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,9 @@ import { HttpService } from "./shared/services/http.service"
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
