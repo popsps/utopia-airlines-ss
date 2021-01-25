c964feac4873ca3f22523bd4aa92924cbf66b95b
Wesley Clements <wesley.clements@gmail.com>
2021-01-25 12:31:56 -0800

    updated import

--- backend/bookings/routes/api/bookings/index.js
+++ backend/bookings/routes/api/bookings/index.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 
-const { bookingController } = require("../../../controllers/bookingController");
+const { bookingController } = require("../../../controllers");
 
 const { requireAuthentication } = require("@utopia-airlines-wss/common/middleware");
 
