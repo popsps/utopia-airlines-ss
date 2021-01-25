fd313fa53770e3738f5d8266c92b4b51cec71454
Wesley Clements <wesley.clements@gmail.com>
2021-01-25 12:24:43 -0800

    moved  guest and user booking functionality into dedicated services

--- backend/bookings/services/bookingService.js
+++ backend/bookings/services/bookingService.js
@@ -1,7 +1,8 @@
-const { Op, Model } = require("sequelize");
 const { sequelize } = require("@utopia-airlines-wss/common/db");
-const { Booking, UserBooking, GuestBooking, Flight } = require("@utopia-airlines-wss/common/models");
-const { StandardizedError, NotFoundError, BadRequestError, handleMutationError, AuthorizationError   } = require("@utopia-airlines-wss/common/errors");
+const { Booking } = require("@utopia-airlines-wss/common/models");
+const {  NotFoundError,  handleMutationError   } = require("@utopia-airlines-wss/common/errors");
+
+const { userBookingService, guestBookingService } = require("./");
 
 const findBookingById = async (id, options) => {
   const booking = await Booking.findByPk(id, options);
@@ -10,32 +11,26 @@ const findBookingById = async (id, options) => {
 };
 
 const bookingService = {
-  async findAllBookings({ isActive = true, userId } = {}) {
-    const useUserBooking = userId != null;
+  async findAllBookings({ isActive = true } = {}) {
     const where = { isActive };
-    if (useUserBooking) where.userId = userId;
-    return await (useUserBooking ? UserBooking : Booking).findAll({
+    return await Booking.findAll({
       where,
       include: [
-        ...(useUserBooking
-          ? [ "agent", "user" ]
-          : [
-            {
-              association: "agent",
-              include: "agent",
-            },
-            {
-              association: "user",
-              include: "user",
-            },
-            "guest",
-          ]),
+        {
+          association: "agent",
+          include: "agent",
+        },
+        {
+          association: "user",
+          include: "user",
+        },
+        "guest",
         "flights",
         "passengers",
       ],
     });
   },
-  async findBookingById({ id, userId }) {
+  async findBookingById({ id }) {
     const booking = await findBookingById(
       id,
       {
@@ -54,67 +49,11 @@ const bookingService = {
         ],
       }
     );
-    if (userId != null && booking.user?.userId !== userId)
-      throw new AuthorizationError();
     return booking;
   },
-  async findGuestBookingById({ id }) {
-    const booking = await GuestBooking.findByPk(
-      id,
-      {
-        include: [
-          "agent",
-          "flights",
-          "passengers",
-        ],
-      }
-    );
-    if (!booking) throw new NotFoundError("cannot find booking");
-    return booking;
-  },
-  async createBooking({ flights, passengers, contact, userId, agent }) {
-    const transaction = await sequelize.transaction();
-    try {
-      const fullFlightCount = await Flight.count({
-        where: {
-          id: { [Op.or]: flights },
-          availableSeats: { [Op.lt]: passengers.length },
-        },
-      });
-      if (fullFlightCount)
-        throw new BadRequestError("not enough seats");
-      const data = {
-        passengers: passengers.map(
-          ({ name: { given: givenName, family: familyName } = {}, ...rest }) => ({ ...rest,  givenName, familyName })
-        ),
-      };
-      if (agent != null) data.agent = { agentId: agent.id };
-      if (userId != null) data.user = { userId: contact };
-      else data.guest = {
-        contactEmail: contact.email,
-        contactPhone: contact.phone,
-      };
-      const booking = await Booking.create(
-        data,
-        {
-          transaction,
-          include: [
-            "passengers",
-            "agent",
-            typeof contact === "number" || contact instanceof Model
-              ? "user"
-              : "guest",
-          ],
-        }
-      );
-      await booking.setFlights(flights, { transaction });
-      await transaction.commit();
-      return booking;
-    } catch (err) {
-      await transaction.rollback();
-      if (err instanceof StandardizedError) throw err;
-      handleMutationError(err);
-    }
+  async createBooking({ flights, passengers, contact, userId, agentId }) {
+    if (userId == null) return guestBookingService.createGuestBooking({ flights, passengers, contact, agentId });
+    else return userBookingService.createUserBooking({ flights, passengers, userId, agentId });
   },
   async updateBooking(id, { isActive }) {
     const booking = await findBookingById(id);
---
+++ backend/bookings/services/guestBookingService.js
@@ -0,0 +1,87 @@
+const { sequelize } = require("@utopia-airlines-wss/common/db");
+const { Booking, GuestBooking } = require("@utopia-airlines-wss/common/models");
+const { StandardizedError, NotFoundError, handleMutationError   } = require("@utopia-airlines-wss/common/errors");
+
+const { flightService } = require("./");
+
+const guestBookingService = {
+  async findAllGuestBookings({ isActive = true } = {}) {
+    const where = { isActive };
+    return await GuestBooking.findAll({
+      where,
+      include: [
+        "agent",
+        "flights",
+        "passengers",
+      ],
+    });
+  },
+  async findGuestBookingById({ id }) {
+    const booking = await GuestBooking.findByPk(
+      id,
+      {
+        include: [
+          "agent",
+          "flights",
+          "passengers",
+        ],
+      }
+    );
+    if (!booking) throw new NotFoundError("cannot find booking");
+    return booking;
+  },
+  async createGuestBooking({ flights, passengers, contact, agentId }) {
+    const transaction = await sequelize.transaction();
+    try {
+      await flightService.validateFlights({ flights, passengerCount: passengers.length });
+      const data = {
+        guest: {
+          contactEmail: contact.email,
+          contactPhone: contact.phone,
+        },
+        passengers: passengers.map(
+          ({ name: { given: givenName, family: familyName } = {}, ...rest }) => ({ ...rest,  givenName, familyName })
+        ),
+      };
+      if (agentId != null) data.agent = { agentId };
+      const booking = await Booking.create(
+        data,
+        {
+          transaction,
+          include: [
+            "passengers",
+            "agent",
+            "guest",
+          ],
+        }
+      );
+      await booking.setFlights(flights, { transaction });
+      await transaction.commit();
+      return booking;
+    } catch (err) {
+      await transaction.rollback();
+      if (err instanceof StandardizedError) throw err;
+      handleMutationError(err);
+    }
+  },
+  async updateBooking(id, { isActive }) {
+    const booking = await GuestBooking.findByPk(id);
+    if (!booking) throw new NotFoundError("cannot find booking");
+    const transaction = await sequelize.transaction();
+    try {
+      await booking.update({ 
+        isActive,
+      }, {
+        transaction,
+      });
+      await transaction.commit();
+      return booking;
+    } catch (err) {
+      await transaction.rollback();
+      handleMutationError(err);
+    }
+
+  },
+};
+
+module.exports = { guestBookingService };
