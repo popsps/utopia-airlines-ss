383a457ddde673281312b766bd79848fc3585ff8
Wesley Clements <wesley.clements@gmail.com>
2021-01-25 12:32:15 -0800

    refactored to use user/guest booking controllers

--- backend/bookings/routes/api/bookings/guest/index.js
+++ backend/bookings/routes/api/bookings/guest/index.js
@@ -1,45 +1,46 @@
 const { Router } = require("express");
 const router = Router();
 
-const { bookingController } = require("../../../../controllers/bookingController");
+const { guestBookingController } = require("../../../../controllers");
 
 const { body } = require("express-validator");
 const { validateRequest, requireAuthentication } = require("@utopia-airlines-wss/common/middleware");
 
 
-router.get("/",
-  requireAuthentication({
-    roles: ["ADMIN", "AGENT"],
-  }),
-  bookingController.getAll
-);
-router.post("/",
-  [
-    body("flights")
-      .exists().withMessage("please provide flights")
-      .bail()
-      .isArray().withMessage("flights must be an array")
-      .isLength({ min:1 }).withMessage("please provide a flight"),
-    body("passengers")
-      .exists().withMessage("please provide passengers")
-      .bail()
-      .isArray().withMessage("passengers must be an array")
-      .isLength({ min:1 }).withMessage("please provide a passenger"),
-    body("contact.email")
-      .exists().withMessage("please provide an email")
-      .bail()
-      .isEmail().withMessage("please provide a valid email"),
-    body("contact.phone")
-      .exists().withMessage("please provide a phone")
-      .bail()
-      .isMobilePhone().withMessage("please provide a valid phone number"),
-  ],
-  validateRequest,
-  bookingController.create
-);
+router.route("/")
+  .get(
+    requireAuthentication({
+      roles: ["ADMIN", "AGENT"],
+    }),
+    guestBookingController.getAll
+  )
+  .post(
+    [
+      body("flights")
+        .exists().withMessage("please provide flights")
+        .bail()
+        .isArray().withMessage("flights must be an array")
+        .isLength({ min:1 }).withMessage("please provide a flight"),
+      body("passengers")
+        .exists().withMessage("please provide passengers")
+        .bail()
+        .isArray().withMessage("passengers must be an array")
+        .isLength({ min:1 }).withMessage("please provide a passenger"),
+      body("contact.email")
+        .exists().withMessage("please provide an email")
+        .bail()
+        .isEmail().withMessage("please provide a valid email"),
+      body("contact.phone")
+        .exists().withMessage("please provide a phone")
+        .bail()
+        .isMobilePhone().withMessage("please provide a valid phone number"),
+    ],
+    validateRequest,
+    guestBookingController.create
+  );
 
 router.route("/:id")
-  .get(bookingController.getById)
+  .get(guestBookingController.getById)
   .put(
     [
       body("contact.email")
@@ -48,11 +49,7 @@ router.route("/:id")
         .isMobilePhone().withMessage("please provide a valid phone number"),
     ],
     validateRequest,
-    bookingController.updateById
-  )
-  .delete(
-    requireAuthentication(),
-    bookingController.deleteById
+    guestBookingController.updateById
   );
 
 module.exports = router;
