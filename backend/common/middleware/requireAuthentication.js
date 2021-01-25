06e9384530909edc9affc7c0a26e67a44ae1fa1e
Wesley Clements <wesley.clements@gmail.com>
2021-01-25 11:29:00 -0800

    refactored to clarify logic

--- backend/common/middleware/requireAuthentication.js
+++ backend/common/middleware/requireAuthentication.js
@@ -1,12 +1,18 @@
 const { AuthenticationError, AuthorizationError } = require("../errors");
 
-const requireAuthentication = ({ roles, errorMessage, condition = true } = {}) => (req, res, next) => {
-  if ((typeof condition === "function"? condition(req): condition)) {
-    if (!req.user) throw new AuthenticationError();
-    if (roles && !roles.includes(req.user.role.name)) 
-      throw new AuthorizationError(errorMessage);
-  }
-  next();
+const requireAuthentication = ({ roles, errorMessage, condition = true } = {}) => {
+  const shouldCheckAuthentication = typeof condition === "function"
+    ? condition
+    : () => condition;
+  const checkRole = (role) => roles && !roles.includes(role);
+  return(req, res, next) => {
+    if (shouldCheckAuthentication(req)) {
+      if (!req.user) throw new AuthenticationError();
+      if (checkRole(req.user?.role.name)) 
+        throw new AuthorizationError(errorMessage);
+    }
+    next();
+  };
 };
 
 module.exports = { requireAuthentication };
