6ceb568249e6cd8935fdc0a20ebca50fd0a24cec
Wesley Clements <wesley.clements@gmail.com>
2021-01-28 13:15:15 -0800

    added authenication to routes

--- backend/flights/routes/api/flights/index.js
+++ backend/flights/routes/api/flights/index.js
@@ -1,20 +1,28 @@
+
 const { Router } = require("express");
+const { requireAuthentication } = require("@utopia-airlines-wss/common/middleware");
+
 const router = Router();
 
 const { flightController } = require("../../../controller");
 
-router.get("/", flightController.getAll);
+router.route("/")
+  .get(flightController.getAll)
+  .all(requireAuthentication({ roles: ["ADMIN"] }))
+  .post(flightController.create);
 
-router.post("/", flightController.create);
+router.route("/:id")
+  .get(flightController.getById)
+  .all(requireAuthentication({ roles: ["ADMIN"] }))
+  .put(flightController.updateById)
+  .delete(flightController.deleteById);
 
-router.get("/:id", flightController.getById);
+router.route("/:id/bookings")
+  .all(requireAuthentication({ roles: ["ADMIN"] }))
+  .get(flightController.getFlightBookings);
 
-router.put("/:id", flightController.updateById);
-
-router.delete("/:id", flightController.deleteById);
-
-router.get("/:id/bookings", flightController.getFlightBookings);
-
-// router.get("/:id/passengers", flightController.getFlightPassengers);
+// router.route("/:id/passengers")
+//   .all(requireAuthentication({ roles: ["ADMIN"] }))
+//   .get(flightController.getFlightPassengers);
 
 module.exports = router;
