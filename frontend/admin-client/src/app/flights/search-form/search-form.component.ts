48860d455b454f3cb7cf6c9b1a3aff0fb2eb0b39
Wesley Clements <wesley.clements@gmail.com>
2021-01-31 14:01:42 -0800

    switched to using reactive forms

--- frontend/admin-client/src/app/flights/search-form/search-form.component.ts
+++ frontend/admin-client/src/app/flights/search-form/search-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { FlightFilter } from "../../shared/models/FlightFilter";
 
 @Component({
@@ -7,11 +8,63 @@ import { FlightFilter } from "../../shared/models/FlightFilter";
   styleUrls: ['./search-form.component.scss']
 })
 export class SearchFormComponent implements OnInit {
-  @Output() filter: FlightFilter = {};
+  filterFormControls: FormGroup;
+  filter: FlightFilter = {
+    departureDateRange: []
+  };
   @Output() filterChanged = new EventEmitter<FlightFilter>();
-  constructor() { };
+  constructor(private formBuilder: FormBuilder) { };
 
   ngOnInit(): void {
+    this.filterFormControls = this.formBuilder.group({
+      origin: [""],
+      destination: [""],
+      departureDateRange: this.formBuilder.group({
+        start: [""],
+        end: new FormControl(""),
+      })
+    });
+    this.filterFormControls.valueChanges.subscribe(({ origin, destination, departureDateRange }) => {
+      let changed = this.setOrigin(origin.toUpperCase())
+        || this.setDestination(destination.toUpperCase())
+        || this.setDepartureDateRange(departureDateRange);
+      if (changed) this.filterChanged.emit(this.filter);
+    });
+  }
+
+  setOrigin(origin: string): boolean {
+    if (this.filter.origin !== origin)
+    {
+      this.filter.origin = origin;
+      return true;
+    }
+    return false;
+  }
+
+  setDestination(destination: string): boolean {
+    if (this.filter.destination !== destination)
+    {
+      this.filter.destination = destination;
+      return true;
+    }
+    return false;
+  }
+
+  setDepartureDateRange(departureDateRange): boolean {
+    let updated = false;
+    const start = new Date(departureDateRange.start);
+    const end = new Date(departureDateRange.end);
+    if (start.getTime() !== this.filter.departureDateRange[0]?.getTime())
+    {
+      this.filter.departureDateRange[0] = start;
+      updated = true;
+    }
+    if (end.getTime() !== this.filter.departureDateRange[1]?.getTime())
+    {
+      this.filter.departureDateRange[0] = end;
+      updated = true;
+    }
+    return updated;
   }
 
 }
