cc8cd4a3b5a3787e72d97ad964f25101289dc6ec
Wesley Clements <wesley.clements@gmail.com>
2021-01-31 08:49:27 -0800

    parsed hex string sub to int

--- backend/common/middleware/currentUser.js
+++ backend/common/middleware/currentUser.js
@@ -7,7 +7,7 @@ const getCurrentUser = async (req, res, next) => {
   try {
     if (req.session?.jwt) {
       const payload = await authJWTHandler.verifyJWT(req.session.jwt);
-      req.user = await User.findByPk(payload.sub, { include: ["role"] });
+      req.user = await User.findByPk(Number.parseInt(payload.sub, 16), { include: ["role"] });
     }
     next();
   } catch (err) {
