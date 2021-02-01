c4c7cf3af3569f0958b55f43a9ff26303210ae78
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2021-02-01 14:03:27 -0500

    add test files

---
+++ backend/authentication-microservice/src/test/java/com/ss/utopia/auth/controller/SessionControllerTest.java
@@ -0,0 +1,38 @@
+package com.ss.utopia.auth.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionControllerTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void login() {
+  }
+
+  @Test
+  void loginAdmin() {
+  }
+
+  @Test
+  void loginAgent() {
+  }
+
+  @Test
+  void getSession() {
+  }
+
+  @Test
+  void logout() {
+  }
+}
---
+++ backend/authentication-microservice/src/test/java/com/ss/utopia/auth/service/UserServiceTest.java
@@ -0,0 +1,52 @@
+package com.ss.utopia.auth.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+  @Test
+  void loadUserByUsername() {
+  }
+
+  @Test
+  void loadAuthenticatedUser() {
+  }
+
+  @Test
+  void signup() {
+  }
+
+  @Test
+  void updateUser() {
+  }
+
+  @Test
+  void deleteUser() {
+  }
+
+  @Test
+  void findUserById() {
+  }
+
+  @Test
+  void verifyOwnershipAndReturnOwner() {
+  }
+
+  @Test
+  void removeCookie() {
+  }
+
+  @Test
+  void getUserByUsername() {
+  }
+
+  @Test
+  void isUserAdmin() {
+  }
+
+  @Test
+  void isUserAgent() {
+  }
+}
