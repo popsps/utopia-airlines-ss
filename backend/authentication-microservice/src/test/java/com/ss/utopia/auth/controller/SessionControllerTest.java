c9426aca4a5e0e077cc83dc7acfcdab6aef564fb
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2021-02-01 14:43:35 -0500

    add displayName base test

--- backend/authentication-microservice/src/test/java/com/ss/utopia/auth/controller/SessionControllerTest.java
+++ backend/authentication-microservice/src/test/java/com/ss/utopia/auth/controller/SessionControllerTest.java
@@ -1,12 +1,23 @@
 package com.ss.utopia.auth.controller;
 
+import com.ss.utopia.auth.service.UserService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(SessionController.class)
 class SessionControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+  @MockBean
+  private UserService userService;
 
   @BeforeEach
   void setUp() {
@@ -17,22 +28,27 @@ class SessionControllerTest {
   }
 
   @Test
+  @DisplayName("Test login")
   void login() {
   }
 
   @Test
+  @DisplayName("Test login as an admin")
   void loginAdmin() {
   }
 
   @Test
+  @DisplayName("Test login as an agent")
   void loginAgent() {
   }
 
   @Test
+  @DisplayName("Test get session iformation")
   void getSession() {
   }
 
   @Test
+  @DisplayName("Test logout")
   void logout() {
   }
 }
