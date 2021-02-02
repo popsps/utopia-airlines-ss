f4a37655fc9e0310ddef3cbda9ff32975570b5d9
Wesley Clements <wesley.clements@gmail.com>
2021-02-01 20:37:19 -0800

    updated to reflect changes on backend

--- frontend/admin-client/src/app/shared/models/Flight.ts
+++ frontend/admin-client/src/app/shared/models/Flight.ts
@@ -3,7 +3,9 @@ import { Route } from './Route';
 
 export class Flight implements Deserializable {
   id: bigint;
+  routeId: number;
   route: Route;
+  airplaneId: number;
   departureTime: Date;
   maxCapacity: number;
   reservedSeats: number;
@@ -12,10 +14,11 @@ export class Flight implements Deserializable {
   seatPrice: number;
 
   deserialize(input: any): this {
-    const { routeId, route, departureTime, ...rest } = input;
+    const { route, departureTime, ...rest } = input;
     Object.assign(this, rest);
     this.route = new Route().deserialize(route);
     this.departureTime = new Date(departureTime);
+    this.departureTime.setFullYear(2021);
     return this;
   }
 }
