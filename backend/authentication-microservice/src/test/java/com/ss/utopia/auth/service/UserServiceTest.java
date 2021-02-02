91c62db14b1e4295286dc7de9b06b15eb7c85f06
Wesley Clements <wesley.clements@gmail.com>
2021-02-02 08:10:54 -0800

    added tests for getUserById

--- backend/authentication-microservice/src/test/java/com/ss/utopia/auth/service/UserServiceTest.java
+++ backend/authentication-microservice/src/test/java/com/ss/utopia/auth/service/UserServiceTest.java
@@ -13,8 +13,13 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.client.HttpServerErrorException;
 
 import javax.servlet.http.Cookie;
+
+import java.util.Collection;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -39,9 +44,8 @@ public class UserServiceTest {
   @Test
   @DisplayName("Test loading Authenticated User")
   void loadAuthenticatedUser() throws Exception {
-    User expectedUser =
-      new User(32434L, "adming32434", "password", new UserRole(1L, "ADMIN"),
-        "fname", "lname", "flcust@gmail.com", "2028769868");
+    User expectedUser = new User(32434L, "adming32434", "password", new UserRole(1L, "ADMIN"), "fname", "lname",
+        "flcust@gmail.com", "2028769868");
     Mockito.when(userDao.findByUsername(Mockito.any())).thenReturn(Optional.of(expectedUser));
     User actualUser = userService.loadAuthenticatedUser("adming32434", "password").orElseThrow(Exception::new);
     assertEquals(expectedUser, actualUser);
@@ -60,7 +64,13 @@ public class UserServiceTest {
   }
 
   @Test
-  void findUserById() {
+  void getUserById() {
+    final User testUser = new User(1l, "admin32434", "password", new UserRole(1L, "ADMIN"), "fname", "lname",
+        "flcust@gmail.com", "2028769868");
+    Mockito.when(userDao.findById(1l)).thenReturn(Optional.of(testUser));
+    Mockito.when(userDao.findById(2l)).thenReturn(Optional.empty());
+    assertEquals(userService.getUserById(1l), testUser);
+    assertThrows(HttpServerErrorException.class, () -> userService.getUserById(2l));
   }
 
   @Test
@@ -81,9 +91,8 @@ public class UserServiceTest {
   @Test
   @DisplayName("Mock Test getting a user by username from DB")
   void getUserByUsername() {
-    User expectedUser =
-      new User(32434L, "adming32434", "password", new UserRole(1L, "ADMIN"),
-        "fname", "lname", "flcust@gmail.com", "2028769868");
+    User expectedUser = new User(32434L, "adming32434", "password", new UserRole(1L, "ADMIN"), "fname", "lname",
+        "flcust@gmail.com", "2028769868");
     Mockito.when(userDao.findByUsername(Mockito.any())).thenReturn(Optional.of(expectedUser));
     User actualUser = userService.getUserByUsername("adming32434");
     assertEquals(expectedUser, actualUser);
@@ -92,9 +101,8 @@ public class UserServiceTest {
   @Test
   @DisplayName("Test whether is a user is an admin. should return true")
   void isUserAdmin() {
-    User user =
-      new User(32434L, "adming32434", "password", new UserRole(1L, "ADMIN"),
-        "fname", "lname", "flcust@gmail.com", "2028769868");
+    User user = new User(32434L, "adming32434", "password", new UserRole(1L, "ADMIN"), "fname", "lname",
+        "flcust@gmail.com", "2028769868");
     boolean actualIsAdmin = userService.isUserAdmin(user);
     assertEquals(true, actualIsAdmin);
   }
@@ -102,9 +110,8 @@ public class UserServiceTest {
   @Test
   @DisplayName("Test whether is a user is an agent. should return true")
   void isUserAgent() {
-    User user =
-      new User(32434L, "agent32434", "password", new UserRole(3L, "AGENT"),
-        "fname", "lname", "flcust@gmail.com", "2028769868");
+    User user = new User(32434L, "agent32434", "password", new UserRole(3L, "AGENT"), "fname", "lname",
+        "flcust@gmail.com", "2028769868");
     boolean actualIsAgent = userService.isUserAgent(user);
     assertEquals(true, actualIsAgent);
   }
