204c855a1877d21140f2a6bf3b476db2f8c0e6f5
Samnj583 <samuel.johnson@smoothstack.com>
2021-02-02 23:04:33 -0700

    Added temporary constructor

--- backend/authentication-microservice/src/main/java/com/ss/utopia/auth/entity/User.java
+++ backend/authentication-microservice/src/main/java/com/ss/utopia/auth/entity/User.java
@@ -45,6 +45,18 @@ public class User {
     this.role = role;
   }
 
+  public User(Long id, String username, String password, UserRole role, String givenName, String familyName,
+  String email, String phone) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.role = role;
+    this.givenName = givenName;
+    this.familyName = familyName;
+    this.email = email;
+    this.phone = phone;
+  }
+
   public User(String username, String password, UserRole role, String givenName, String familyName,
       String email, String phone) {
     this.username = username;
