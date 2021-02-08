69394600e3e7c5638fbee3775f55b34298236341
Wesley Clements <wesley.clements@gmail.com>
2021-02-08 09:16:01 -0800

    fixed arrays not using replacer

--- backend/common/util/createReplacer.js
+++ backend/common/util/createReplacer.js
@@ -1,5 +1,11 @@
-const createReplacer = ({ type = "slim" }) => (_key, value) =>  value?.toJSON?.length === 1
-  ? value.map((passenger) => passenger.toJSON(type))
-  : value;
+const createReplacer = ({ type = "slim" }) => {
+  const replacer = (_key, value) =>  {
+    if (Array.isArray(value)) return value.map((value) => replacer(null, value));
+    return value?.toJSON?.length === 1
+      ? value.toJSON(type)
+      : value;
+  };
+  return replacer;
+};
 
 module.exports = { createReplacer };
