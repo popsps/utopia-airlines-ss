8949f73aa18ef9c7db292735f1d3b4074065526a
Wesley Clements <wesley.clements@gmail.com>
2021-02-09 11:21:35 -0800

    updated to reflect new backend result

--- frontend/admin-client/src/app/shared/services/flight.service.ts
+++ frontend/admin-client/src/app/shared/services/flight.service.ts
@@ -6,16 +6,26 @@ import { map } from 'rxjs/operators';
 import { environment } from "../../../environments/environment";
 import { Flight } from "../models/Flight";
 
+export interface PaginatedFlightResult {
+  total: number;
+  offset: number;
+  count: number,
+  results: Flight[];
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class FlightService {
-  private static parseFlights: OperatorFunction<any, Flight[]> = map((flights: any) => flights.map(obj => new Flight().deserialize(obj)));
+  private static parseFlights: OperatorFunction<any, PaginatedFlightResult> = map(({ results, ...rest }: any) => ({
+    ...rest,
+    results: results.map(obj => new Flight().deserialize(obj))
+  }));
   constructor(private http: HttpClient) { }
 
-  getAll(): Observable<Flight[]> {
+  getAll(): Observable<PaginatedFlightResult> {
     return FlightService.parseFlights(this.http.get(environment.flightApiUrl));
-  }
+  };
 
   post(flight: any) {
     return this.http.post(environment.flightApiUrl, flight, { headers: { "Content-Type": "application/json" } });
