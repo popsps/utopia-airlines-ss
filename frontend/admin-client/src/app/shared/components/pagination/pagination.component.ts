14a010ba5b1d87be1220164ddd0cdd4fc3b39c2d
Wesley Clements <wesley.clements@gmail.com>
2021-02-09 12:36:31 -0800

    refactored input names to better reflect purpose

--- frontend/admin-client/src/app/shared/components/pagination/pagination.component.ts
+++ frontend/admin-client/src/app/shared/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -7,10 +7,10 @@ import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 })
 export class PaginationComponent implements OnInit {
 
-  @Input() collectionSize: number;
-  @Input() maxSize: number;
+  @Input() currentPage: number;
+  @Input() totalPageCount: number;
+  @Input() visiblePageCount: number;
   @Input() boundaryLinks: boolean;
-  @Input() page: number;
   @Output() pageChange = new EventEmitter<number>(true);
 
   pages: number[];
@@ -20,18 +20,18 @@ export class PaginationComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.pages = Array(this.collectionSize).fill(0).map((v, i) => i + 1);
+    this.pages = Array(this.totalPageCount).fill(0).map((v, i) => i + 1);
     this.buildActivePages();
-    console.log('pages', this.collectionSize);
+    console.log('pages', this.totalPageCount);
     this.pageChange.subscribe(p => this.buildActivePages());
   }
 
   goNextPage(): void {
-    this.pageChange.emit(this.page + 1);
+    this.pageChange.emit(this.currentPage + 1);
   }
 
   goPrevPage(): void {
-    this.pageChange.emit(this.page - 1);
+    this.pageChange.emit(this.currentPage - 1);
   }
 
   goFirstPage(): void {
@@ -39,35 +39,40 @@ export class PaginationComponent implements OnInit {
   }
 
   goLastPage(): void {
-    this.pageChange.emit(this.collectionSize);
+    this.pageChange.emit(this.totalPageCount);
   }
 
   goToPage(n: number): void {
     this.pageChange.emit(n);
+    console.log(n);
   }
 
   private buildActivePages(): void {
     // if number of pages to show is less than the collection size
-    if (this.maxSize > this.collectionSize) {
+    if (this.visiblePageCount > this.totalPageCount)
+    {
       this.activePages = this.pages;
       return;
     }
     this.activePages = [];
-    let size = this.maxSize;
-    const currentPage = this.page;
+    let size = this.visiblePageCount;
+    const currentPage = this.currentPage;
     this.activePages.push(currentPage);
     size--;
     let prev = (currentPage > 1) ? currentPage - 1 : null;
-    let next = (currentPage < this.collectionSize) ? currentPage + 1 : null;
-    while (size !== 0) {
-      if (prev) {
+    let next = (currentPage < this.totalPageCount) ? currentPage + 1 : null;
+    while (size !== 0)
+    {
+      if (prev)
+      {
         this.activePages.push(prev);
         prev = (prev > 1) ? prev - 1 : null;
         size--;
       }
-      if (next) {
+      if (next)
+      {
         this.activePages.push(next);
-        next = (next < this.collectionSize) ? next + 1 : null;
+        next = (next < this.totalPageCount) ? next + 1 : null;
         size--;
       }
     }
