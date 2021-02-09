1e2f57fb0c51ad6275309647b5b5ec7223f9aed7
Wesley Clements <wesley.clements@gmail.com>
2021-02-09 14:34:50 -0800

    updated to include properties associated with symbols

--- backend/common/util/removedUndefined.js
+++ backend/common/util/removedUndefined.js
@@ -1,5 +1,9 @@
 const removeUndefined = query => {
-  return Object.entries(query)
+  return [
+    ...Object.entries(query),
+    ...Object.getOwnPropertySymbols(query)
+      .map(key=> [key, query[key]]),
+  ]
     .filter(([,value]) => value != null)
     .reduce((obj, [key, value]) => {
       obj[key] = value;
