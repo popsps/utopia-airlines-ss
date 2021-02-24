4b6a2b7fdcc95f25f983f9a9655f289a042b6021
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2021-02-24 08:22:58 -0500

    base changes for integrating admin portal to the cloud

--- frontend/admin-client/src/environments/environment.prod.ts
+++ frontend/admin-client/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true,
+  utopiaAirlineApi: 'http://localhost:8081',
+  sessionInfoUrl: '/api/session',
+  loginUrl: '/api/session/admin',
+  userApiUrl: '/api/users',
+  bookingApiUrl: '/api/bookings',
+  flightApiUrl: '/api/flights',
+  passengerApiUrl: '/api/passengers',
 };
