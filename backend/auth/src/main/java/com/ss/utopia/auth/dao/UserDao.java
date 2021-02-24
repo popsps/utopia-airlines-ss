7f801e0e997375fd690adb2e9d22e11752b33c80
Samnj583 <samuel.johnson@smoothstack.com>
2021-02-24 09:07:17 -0700

    Paging and Filtering

--- backend/auth/src/main/java/com/ss/utopia/auth/dao/UserDao.java
+++ backend/auth/src/main/java/com/ss/utopia/auth/dao/UserDao.java
@@ -2,12 +2,27 @@ package com.ss.utopia.auth.dao;
 
 
 import com.ss.utopia.auth.entity.User;
+import com.ss.utopia.auth.entity.UserRole;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserDao extends JpaRepository<User, Long> {
   Optional<User> findByUsername(String username);
+
+  Page<User> findByRoleNameAndUsernameAndEmailContaining(String role, String username, String email, Pageable pageable);
+  Page<User> findByRoleNameAndUsernameContaining(String role,String username, Pageable pageable);
+  Page<User> findByRoleNameAndEmailContaining(String role, String email, Pageable pageable);
+  Page<User> findByUsernameAndEmailContaining(String username, String email, Pageable pageable);
+  Page<User> findByRoleNameContaining(String role, Pageable pageable);
+  Page<User> findByUsernameContaining(String username, Pageable pageable);
+  Page<User> findByEmailContaining(String email, Pageable pageable);
+  Page<User> findAll(Pageable pageable);
 }
--- frontend/admin-client/src/app/users/users.component.ts
+++ frontend/admin-client/src/app/users/users.component.ts
@@ -6,8 +6,8 @@ import {
 } from '@angular/forms';
 import { environment } from '../../environments/environment';
 import { HttpService } from '../shared/services/http.service';
-import { User } from '../shared/models/user';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { PagerService } from '../shared/services/pager.service';
 
 @Component({
   selector: 'app-users',
@@ -15,10 +15,12 @@ import { User } from '../shared/models/user';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-  // TODO: Add Typescript type instead of any
+  page = 1;
+  limit = 15;
+  totalUsers: number;
+  pager: any = {};
+  result: any;
   users: any;
-  searchString: string;
-  searchUsersForm: FormGroup;
   addUserForm: FormGroup;
   username: string;
   password: string;
@@ -29,31 +31,97 @@ export class UsersComponent implements OnInit {
   role: string;
   apiUrl: string;
   isError: boolean;
-  // TODO: Create Error Entity
   error: any;
+  isSearching: boolean;
+  searchUrl = "";
 
   constructor(
     private userService: HttpService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private pagerService: PagerService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.apiUrl = environment.userApiUrl;
     this.isError = false;
+    this.isSearching = false;
     this.initializeUsers();
     this.initializeForm();
   }
 
+  setPage(page: number): void {
+    if (!this.totalUsers || page < 1 || page > this.pager.totalUsers) {
+      return;
+    }
+    this.pager = this.pagerService.getPager(this.totalUsers, page, this.limit);
+    console.log('pager server:', this.pager);
+    console.log('paged users', this.users);
+  }
+
+  navigate(page: number): void {
+    if (page < 1 || page > this.totalUsers / this.limit) {
+      this.isError = true;
+      this.error = {
+        message: "invalid page"
+      };
+      return;
+    }
+    this.page = page;
+    this.initializeUsers();
+  }
+
   initializeUsers() {
+    const offset = this.page - 1;
     this.userService
-      .get(this.apiUrl)
+      .get(this.apiUrl + "?offset=" + offset.toString() + "&limit=" + this.limit.toString() + this.searchUrl)
       .subscribe((res) => {
         this.isError = false;
-        this.users = res;
+        this.result = res;
+        this.users = this.result.content;
+        this.totalUsers = this.result.totalElements;
+        this.setPage(this.page);
       }, (err) => {
         this.isError = true;
         this.error = err.error;
       });
+  }
+
+  searchUsers(usernameFilter, emailFilter, roleFilter) {
+    this.page = 1;
+    const offset = this.page - 1;
+    this.searchUrl = "";
+    if (usernameFilter) {
+      this.searchUrl = this.searchUrl.concat("&username=" + usernameFilter);
+    }
+    if (emailFilter) {
+      this.searchUrl = this.searchUrl.concat("&email=" + emailFilter);
+    }
+    if (roleFilter) {
+      this.searchUrl = this.searchUrl.concat("&role=" + roleFilter);
+    }
+    this.userService
+      .get(this.apiUrl + "?offset=" + offset.toString() + "&limit=" + this.limit.toString() + this.searchUrl)
+      .subscribe((res) => {
+        this.result = res;
+        this.totalUsers = this.result.totalElements;
+        this.users = this.result.content;
+        this.setPage(this.page);
+        this.isError = false;
+        this.isSearching = true;
+      }, (err) => {
+        this.isError = true;
+        this.error = err.error;
+        debugger;
+      });
+  }
+
+  cancelSearch() {
+    this.isSearching = false;
+    this.page = 1;
+    this.searchUrl = "";
+    this.initializeUsers();
   }
 
   initializeForm() {
