cbfd29521eb72b0f3a99bb888acf6932105adaa5
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2021-02-25 08:27:18 -0500

    add prod env for angular app

--- frontend/admin-client/src/environments/environment.prod.ts
+++ frontend/admin-client/src/environments/environment.prod.ts
@@ -1,10 +1,9 @@
 export const environment = {
   production: true,
-  utopiaAirlineApi: 'http://localhost:8081',
-  sessionInfoUrl: '/api/session',
-  loginUrl: '/api/session/admin',
-  userApiUrl: '/api/users',
-  bookingApiUrl: '/api/bookings',
-  flightApiUrl: '/api/flights',
-  passengerApiUrl: '/api/passengers',
+  sessionInfoUrl: 'http://utopiaalb-1704489006.us-east-1.elb.amazonaws.com:8081/api/session',
+  loginUrl: 'http://utopiaALB-1704489006.us-east-1.elb.amazonaws.com:8081/api/session/admin',
+  userApiUrl: 'http://utopiaALB-1704489006.us-east-1.elb.amazonaws.com:8081/api/users',
+  bookingApiUrl: 'http://utopiaALB-1704489006.us-east-1.elb.amazonaws.com:3000/api/bookings',
+  flightApiUrl: 'http://utopiaALB-1704489006.us-east-1.elb.amazonaws.com:3001/api/flights',
+  passengerApiUrl: 'http://utopiaALB-1704489006.us-east-1.elb.amazonaws.com:3000/api/passengers',
 };
