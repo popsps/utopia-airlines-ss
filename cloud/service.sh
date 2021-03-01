bd0c11289f050ef454f493578066e6c35868dff0
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2021-03-01 13:23:15 -0500

    update Jenkinsfile fix nginx support
    Co-authored-by: Samnj583 <Samnj583@users.noreply.github.com>

--- cloud/service.sh
+++ cloud/service.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # mysql -h host_name -P 3306 -u db_master_user -p
+# mysql -u usr -p '-e source file_path.sql'
 # source db.sql
-mysql -h host_name -P 3306 -u "utopia" -p "utopiacoolpassword" mydb < db.baby.sql
+mysql -h utopiards.cs32qiajmyjf.us-east-1.rds.amazonaws.com -P 3306 -u "utopia" -p "utopiacoolpassword" utopia < db.baby.sql
